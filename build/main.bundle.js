eab2074963178de2335e017b93c1d7598c5a1981
Auden Robertson <audenrobertson@gmail.com>
2021-04-08 21:06:38 -0400

    updated index and main to es6 variables

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,17 +1,22 @@
 'use strict';
 
 var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
+    var monthlyRate = 0;
     if (rate) {
-        var monthlyRate = rate / 100 / 12;
+        var _monthlyRate = rate / 100 / 12;
     }
     var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
-    return monthlyPayment;
+    return { principal: principal, years: years, rate: rate, monthlyPayment: monthlyPayment, monthlyRate: monthlyRate };
 };
 
 document.getElementById('calcBtn').addEventListener('click', function () {
     var principal = document.getElementById("principal").value;
     var years = document.getElementById("years").value;
     var rate = document.getElementById("rate").value;
-    var monthlyPayment = calculateMonthlyPayment(principal, years, rate);
-    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+
+    var _calculateMonthlyPaym = calculateMonthlyPayment(principal, years, rate),
+        monthlyPayment = _calculateMonthlyPaym.monthlyPayment,
+        monthlyRate = _calculateMonthlyPaym.monthlyRate;
+
+    document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
 });
