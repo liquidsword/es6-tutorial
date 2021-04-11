4cb37d4f8bc8b61884dd64462e2fe6f522109703
Auden Robertson <audenrobertson@gmail.com>
2021-04-11 15:46:12 -0400

    used promises

--- js/rate-service-mock.js
+++ js/rate-service-mock.js
@@ -10,3 +10,11 @@ let rates = [
         "years": "20"
     }
 ];
+
+export let findAll = () => new Promise((resolve, reject) => {
+    if (rates) {
+        resolve(rates);
+    } else {
+        reject("No rates");
+    }
+});
