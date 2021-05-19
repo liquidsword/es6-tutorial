75917e3f15077a026baf988528b54b15c9586bf9
Auden Robertson <audenrobertson@Audens-MBP.home>
2021-05-19 11:27:35 -0400

    test

---
+++ js/test.js
@@ -0,0 +1,43 @@
+import {
+    users as sampleUsers,
+    breaches as sampleBreaches,
+    response as sampleResponse
+  } from "./sample";
+  
+  function authenticate(email, password) {
+    const account = sampleUsers.find(a => a.email === email);
+    if (account && account.password === password) {
+      return account;
+    } else {
+      return null;
+    }
+  }
+  
+  async function login(email, password) {
+    const account = authenticate(email,      password);
+    if (account) {
+      IsSensitive: false,
+      DataClasses.includes('Passwords'),
+      AddedDate > lastLogin;
+      // A new breach was detected!
+      if (sampleResponse.length > 0) {
+        return {
+          success: true,
+          meta: {
+            suggestPasswordChange: true,
+            // hardcoded for now...
+            breachedAccounts: {name: <Name>, domain: <Domain>, breachDate: <BreachDate>, addedDate: <AddedDate>}
+          }
+        }
+      } else {
+        return { success: true };
+      }
+    } else {
+      return {
+        success: false,
+        message: "The username or password you entered is invalid."
+      };
+    }
+  }
+
+  export default login;
